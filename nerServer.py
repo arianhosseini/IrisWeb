fa5f81f385b0d9a45e0cc26b1d4a952e0227f4e5
Arian <arian.ah@hotmail.com>
2015-12-25 19:06:21 +0330

    going for test

--- nerServer.py
+++ nerServer.py
@@ -11,8 +11,8 @@ import os
 
 
 app = Flask(__name__)
-#tagger = POSTagger(model='resources/postagger.model')
-tagger = ""
+tagger = POSTagger(model='resources/postagger.model')
+
 @app.route("/")
 def index():
     print "hello"
@@ -22,17 +22,25 @@ def index():
 
 @app.route("/ner",methods=['POST'])
 def ner():
-    if request.method == 'POST':
+    if request.method == 'POST':        
         print request.form['text']
+        result = tagger.tag(word_tokenize(request.form['text']))
+        outLine = ""
+        for counter in range(len(result)-1):
+            token = result[counter]
+            outLine += token[0].encode('utf-8') + "\t" + token[1] + "\n"
+
+        outLine += result[-1][0].encode('utf-8') + "\t" + result[-1][1]
+        tempFile = open('evalText.txt','w')
+        tempFile.write(outLine)
+        tempFile.close()
+        os.system("java -jar CoreNLP.jar")
         outputFile = open('result.txt','rb')
-        print "inja"
         result = outputFile.read().decode('utf-8')
         outputFile.close()
-        print result
-        tokens = result.split()
+        tokens = result.split(' ')
         outText = ""
         for token in tokens:
-            print token
             word = ""
             splitedText = token.split('/')
             if len(splitedText) > 1:
@@ -47,39 +55,6 @@ def ner():
 
         print outText
         return outText
-
-        # print request.form['text']
-        # result = tagger.tag(word_tokenize(request.form['text']))
-        # outLine = ""
-        # for counter in range(len(result)-1):
-        #     token = result[counter]
-        #     outLine += token[0].encode('utf-8') + "\t" + token[1] + "\n"
-
-        # outLine += result[-1][0].encode('utf-8') + "\t" + result[-1][1]
-        # tempFile = open('evalText.txt','w')
-        # tempFile.write(outLine)
-        # tempFile.close()
-        # os.system("java -jar CoreNLP.jar")
-        # outputFile = open('result.txt','r')
-        # result = outputFile.read()
-        # outputFile.close()
-        # tokens = result.split(' ')
-        # outText = ""
-        # for token in tokens:
-        #     word = ""
-        #     splitedText = token.split('/')
-        #     if len(splitedText) > 1:
-        #     #print splitedText[0]," ",splitedText[1]
-        #         if splitedText[1] != "O":
-        #             tag = splitedText[1].split('_')
-        #             #   print tag[1].lower()
-        #             word = '<span class="'+ tag[1].lower() +'">' + splitedText[0] + '</span>'
-        #         else:
-        #             word = splitedText[0]
-        #     outText+= word +' '
-
-        # print outText
-        # return outText
 
 
 if __name__ == "__main__":
